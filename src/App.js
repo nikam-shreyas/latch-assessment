f5a27b7f9ac548f9f23089c5e1951d3ffd3ec5fe
nikam-shreyas <shreyas.s.nikam@gmail.com>
2023-12-15 14:45:50 -0500

    Restructuring code to use smarter components

--- src/App.js
+++ src/App.js
@@ -1,55 +1,5 @@
 import "./App.css";
 
-function RenderBase({ item }) {
-  console.log("Base", item);
-  return (
-    <div>
-      {item.children &&
-        item.children.map((child) => {
-          return <RenderChildren item={child} />;
-        })}
-    </div>
-  );
-}
-
-function RenderP({ item }) {
-  console.log("P", item);
-  return (
-    <p style={{ fontWeight: item.bold ? "bold" : "" }}>
-      {item.text}
-      {item.children &&
-        item.children.map((child) => {
-          return <RenderChildren item={child} />;
-        })}
-    </p>
-  );
-}
-
-function RenderH({ item }) {
-  return (
-    <h1>
-      {item.text}
-      {item.children &&
-        item.children.map((child) => {
-          return <RenderChildren item={child} />;
-        })}
-    </h1>
-  );
-}
-
-function RenderUL({ item }) {
-  return (
-    <ul>
-      {item.children &&
-        item.children.map((child) => {
-          return <RenderChildren item={child} />;
-        })}
-    </ul>
-  );
-}
-
-function RenderLI({ item }) {}
-
 const TagFromString = ({ tagName, item }) => {
   const Tag = tagName;
   return (
@@ -78,32 +28,6 @@ function RenderChildren({ item }) {
       {item.children &&
         item.children.map((child) => {
           return <TagFromString tagName={child.type || "span"} item={child} />;
-          // switch (child.type) {
-          //   case "h1":
-          //   case "h2":
-          //   case "h3":
-          //   case "h4":
-          //   case "h5":
-          //   case "h6":
-          //     return <RenderH item={child} />;
-          //   case "text":
-          //     return <>{child.text}</>;
-          //   case "p":
-          //   case "mention":
-          //     return <RenderP item={child} />;
-          //   case "block":
-          //     return <RenderChildren item={child} />;
-          //   case "clause":
-          //     return <RenderChildren item={child} />;
-          //   case "ul":
-          //     return <RenderUL item={child} />;
-          //   case "li":
-          //   case "lic":
-          //     return <RenderLI item={child} />;
-
-          //   default:
-          //     break;
-          // }
         })}
     </>
   );
